2b47c09529f13abe08528a58f729056f01a3bacd
Anubhav311 <anubhavbhambri1@gmail.com>
2019-10-01 15:24:39 +0530

    flushed out specs for challenge.py

--- src/challenge.py
+++ src/challenge.py
@@ -1,6 +1,11 @@
-# Return the "centered" average of an array of ints, 
+# Return the "centered" average of an list of ints, 
 # which we'll say is the mean average of the values, 
-# except ignoring the largest and smallest values in the array. 
+# except ignoring the largest and smallest values in the list.
+# if there are multiple copies of largest or smallest values, ignore only one of min / max values.
+# the input list may be unsorted.
+# you may assume that the list is length 3 or more
+# 
+# you can make your own sort or you can use the built in sort function. 
 
 # centered_average([1, 2, 3, 4, 100]) → 3
 # centered_average([1, 1, 5, 5, 10, 8, 7]) → 5
