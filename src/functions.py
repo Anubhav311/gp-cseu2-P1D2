f93f65b2ce85a2d91f4c9efec77da95f56cc10a3
Anubhav311 <anubhavbhambri1@gmail.com>
2019-10-01 14:52:15 +0530

    worked with functions.py

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,9 @@
 # how to define a function
 
+name = "dave"
+
+def cap(n):
+    return n.capitalize()
 # define a doubling function that passes args by value
 
 
@@ -11,3 +15,7 @@
     # original list is modified
 
 # try out our functions
+
+print(name) # => "dave"
+cap(name) # => "Dave" 
+print(name) # => "dave"
