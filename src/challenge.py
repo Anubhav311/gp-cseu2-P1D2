9a99275702ef9a444975379ee03f17487fedfe75
Anubhav311 <anubhavbhambri1@gmail.com>
2019-10-01 15:45:36 +0530

    completed challenge.py

--- src/challenge.py
+++ src/challenge.py
@@ -4,10 +4,28 @@
 # if there are multiple copies of largest or smallest values, ignore only one of min / max values.
 # the input list may be unsorted.
 # you may assume that the list is length 3 or more
-# 
+
 # you can make your own sort or you can use the built in sort function. 
 
-# centered_average([1, 2, 3, 4, 100]) → 3
-# centered_average([1, 1, 5, 5, 10, 8, 7]) → 5
-# centered_average([-10, -4, -2, -4, -2, 0]) → -3
+def centered_average(arr):
+    sum = 0
+    smallest = arr[0]
+    largest = arr[0]
+
+    for i in arr:
+        if i < smallest:
+            smallest = i
+        if i > largest:
+            largest = i
+
+        sum += i
+
+    sum = sum - smallest - largest
+    avg = int(sum / (len(arr) - 2))
+    return avg
+
+
+print(centered_average([1, 2, 3, 4, 100])) 
+print(centered_average([1, 1, 5, 5, 10, 8, 7]))
+print(centered_average([-10, -4, -2, -4, -2, 0]))
 
